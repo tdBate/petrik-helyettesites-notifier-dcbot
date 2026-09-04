92695b6b819ea941d4034122be8b63450d51bfc8
Bate <td.zazsa@gmail.com>
2026-09-04 16:15:43 +0200

    check for changes

--- server.ts
+++ server.ts
@@ -1,22 +1,38 @@
 import { urlSafeCharacters } from "discord.js";
 import { RootSubstitution, Lesson, Day, Period, Subject, Teacher, Substitution, Teacher2 } from "./models/Models";
+import fs from "node:fs";
 
 const URL_SUBSTITUTIONS: string = "https://filc.petrik.hu/api/timetable/substitutions";
-let substitutionData: RootSubstitution[]
+let substitutionData: RootSubstitution[];
 
-function filterData() {
-    for (let i = substitutionData.length - 1; i >= 0; i--) {
-        if (substitutionData[i].lessons.length == 0) {
-            substitutionData.splice(i,1);
+async function setup() {
+    substitutionData = await JSON.parse(fs.readFileSync("./data/substitutions.json").toString());
+}
+
+function filterData(data:RootSubstitution[]):RootSubstitution[] {
+    for (let i = data.length - 1; i >= 0; i--) {
+        if (data[i].lessons.length == 0) {
+            data.splice(i,1);
         };
     }
+    return data;
 }
 
 async function getData() {
     const response: Response = await fetch(URL_SUBSTITUTIONS);
-    substitutionData = await JSON.parse(await response.text()).data;
+    const new_substitutionData:RootSubstitution[] = filterData(JSON.parse(await response.text()).data);
 
-    filterData();
+    //check for changes
+    if (JSON.stringify(new_substitutionData) != JSON.stringify(substitutionData)) {
+        substitutionData = new_substitutionData;
+        fs.writeFileSync("./data/substitutions.json", JSON.stringify(substitutionData));
+        console.log("Update detected...");
+    }
 }
 
-getData();
+function main() {
+    setup();
+    getData();
+}
+
+main();
