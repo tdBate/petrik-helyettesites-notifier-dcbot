162d7593e877eff05f32d92b69e378c59eee91c1
Bate <td.zazsa@gmail.com>
2026-09-04 17:25:15 +0200

    detect specific changes

--- server.ts
+++ server.ts
@@ -1,12 +1,14 @@
 import { urlSafeCharacters } from "discord.js";
 import { RootSubstitution, Lesson, Day, Period, Subject, Teacher, Substitution, Teacher2 } from "./models/Models";
+import { detailedDiff, DetailedDiff } from "deep-object-diff";
 import fs from "node:fs";
+import { DiffEntry } from "node:util";
 
 const URL_SUBSTITUTIONS: string = "https://filc.petrik.hu/api/timetable/substitutions";
 let substitutionData: RootSubstitution[];
 
 async function setup() {
-    substitutionData = await JSON.parse(fs.readFileSync("./data/substitutions.json").toString());
+    substitutionData= await JSON.parse(fs.readFileSync("./data/substitutions.json").toString());
 }
 
 function filterData(data:RootSubstitution[]):RootSubstitution[] {
@@ -24,6 +26,14 @@ async function getData() {
 
     //check for changes
     if (JSON.stringify(new_substitutionData) != JSON.stringify(substitutionData)) {
+        const changes:DetailedDiff = detailedDiff(substitutionData,new_substitutionData);
+        if (!(Object.keys(changes.added).length === 0)) {
+            console.log(Object.entries(changes.added)[0][1]);
+        }
+        if (!(Object.keys(changes.updated).length === 0)) {
+            //not implemented yet
+        }
+
         substitutionData = new_substitutionData;
         fs.writeFileSync("./data/substitutions.json", JSON.stringify(substitutionData));
         console.log("Update detected...");
