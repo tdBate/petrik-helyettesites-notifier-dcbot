780d349b15e723007e8aa19d15e3a3db4c794b08
Bate <td.zazsa@gmail.com>
2026-09-10 20:36:42 +0200

    remove too much notification protection

--- server.ts
+++ server.ts
@@ -24,7 +24,7 @@ async function getData() {
         //check for changes
         if (JSON.stringify(new_substitutionData) != JSON.stringify(substitutionData)) {
             const changes: RootSubstitution[] = detecChanges(substitutionData, new_substitutionData);
-            if (changes.length>10) {throw new Error("Too much new data");}
+            //if (changes.length>10) {throw new Error("Too much new data");}
             for (let i = 0; i < changes.length; i++) {
                 const shortSub = parseSubstitution(changes[i]);
                 if (shortSub != null) {
